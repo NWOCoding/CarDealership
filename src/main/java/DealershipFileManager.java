076c8843d6358fdb0170c26503e6c13cd35c52aa
OctOcat <willierivas97@gmail.com>
2023-05-12 17:48:45 -0500

    Newest Version

--- src/main/java/DealershipFileManager.java
+++ src/main/java/DealershipFileManager.java
@@ -1,10 +1,11 @@
+import com.opencsv.CSVWriter;
 import java.io.FileWriter;
 
 public class DealershipFileManager {
     public void saveDealership(Dealership dealership) {
         try {
             FileWriter fileWriter = new FileWriter("dealership.csv");
-            
+            CSVWriter csvWriter = new CSVWriter(fileWriter);
         }
     }
 
